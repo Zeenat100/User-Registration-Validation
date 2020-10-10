2c049d36b87d6bd7c264c788c4d89113a7859acf
Zeenat100 <siddzee100@gmail.com>
2020-10-09 19:28:36 -0700

    Add First Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -x
 
 echo "Welcome User For Registration"
+#!/bin/bash -x
+
+echo "Enter your first name"
+read firstName
+pattern="^([a-zA-Z]{3})"
+if [[ $firstName =~ $pattern ]]
+then
+        echo true;
+else
+        echo false;
+fi
 
 
