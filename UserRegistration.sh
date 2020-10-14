05eaccc594726858a8aba77efb02c4f2648239a4
Zeenat100 <siddzee100@gmail.com>
2020-10-14 13:24:48 -0700

    Refactor First Name Validation

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,16 +1,24 @@
-#!/bin/bash -x
 
 echo "Welcome User For Registration"
-#!/bin/bash -x
-
 echo "Enter your first name"
 read firstName
-pattern="^([a-zA-Z]{3})"
-if [[ $firstName =~ $pattern ]]
-then
-        echo true;
-else
-        echo false;
-fi
+function firstName()
+{	local f_name=$1
+	pattern="^([a-zA-Z]{3})"
+	checkPattern $f_name $pattern
+}
+function checkPattern()
+{
+	local value=$1
+	local pattern=$2
+	if [[ $value =~ $pattern ]]
+	then
+        	echo true;
+	else
+        	echo false;
+	fi
+
+}
+firstName $firstName
 
 
