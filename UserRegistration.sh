e69c90c83c2fc4d1cfcd4c25c1ab8eab66799c5b
Zeenat100 <siddzee100@gmail.com>
2020-10-14 17:00:59 -0700

    Add phone validation

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,6 +1,6 @@
 echo "Welcome User For Registration"
-echo "Enter your first name"
-read firstName
+#echo "Enter your first name"
+#read firstName
 function firstName()
 {	local f_name=$1
 	pattern="^([a-zA-Z]{3})"
@@ -29,10 +29,21 @@ function email()
 	pattern="^[0-9a-zA-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2,})$"
 	checkPattern $e_id $pattern
 }
-firstName $firstName
-echo "Enter your last name"
-read lastName
-lastName $lastName
-echo "Enter your email id"
-read email
-email $email
+function phone()
+{
+        local p_id=$1
+        pattern="^([0-9]{2})+([-])+([0-9]{10})"
+        #pattern="^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{10}$"
+	checkPattern $p_id $pattern
+}
+
+#firstName $firstName
+#echo "Enter your last name"
+#read lastName
+#lastName $lastName
+#echo "Enter your email id"
+#read email
+#email $email
+echo "Enter your Phone Number"
+read phone
+phone $phone
